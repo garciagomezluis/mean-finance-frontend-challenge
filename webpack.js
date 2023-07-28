86ccc60fc52c5fdbeb5bb7b2e5ad7a29ec516a7c
Luis García Gómez <garciagomezluis.94@gmail.com>
2023-07-28 02:59:50 -0300

    chore: webpack production bundle

--- webpack.js
+++ webpack.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: {
     app: "./src/index.tsx",
   },
